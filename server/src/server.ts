8dacf1cdf5bd4baa4989e7307a56558d1730843b
RichardFelipe-collab <richardfelipe.123@hotmail.com>
2023-01-18 11:35:58 -0300

    fistr commit

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import fastify from "fastify";
+import { PrismaClient } from '@prisma/client';
+import cors from '@fastify/cors'
+
+const app = fastify()
+
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/hello', async () => {
+  const habits = await prisma.habit.findMany()
+  return habits
+})
+
+app.listen({
+  port: 3333
+})
